433da8935dda568a2c217490436e4483dde6ce86
dev Zeus MAthew <devzeus.mathew721@gmail.com>
2023-09-12 20:08:12 +0530

    REchanged into new file

---
+++ revdigit.js
@@ -0,0 +1,15 @@
+function reverseInteger(num) {
+
+    let numStr = num.toString();
+
+    let reversedStr = numStr.split('');
+
+    reverse().join('');
+
+    let revNum = parseInt(reversedStr,10);
+
+    console.log(reversedStr);
+
+    }
+
+    reverseInteger(45);
